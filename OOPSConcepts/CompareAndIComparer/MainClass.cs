34f966195992f725c40279d549180edfb92f2153
Manish Royh <Manish_Roy@epam.com>
2018-01-17 18:27:18 +0530

    Add project files.

---
+++ OOPSConcepts/CompareAndIComparer/MainClass.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOPSConcepts.CompareAndIComparer
+{
+    public class MainClass
+    {
+        private static List<Person> personList = new List<Person>();
+
+        public static void Processor()
+        {
+            personList.Add(new Person { Age = 10, Name = "Sagar" });
+            personList.Add(new Person { Age = 30, Name = "Manish" });
+            personList.Add(new Person { Age = 6, Name = "Amit" });
+            personList.Add(new Person { Age = 50, Name = "Deepak" });
+
+            Console.WriteLine($"Default Sort using IComparable");
+            personList.Sort();
+            Display(personList);
+
+            Console.WriteLine($"Sort by Age Using IComparer");
+            personList.Sort(new CompareByAge());
+            Display(personList);
+
+            Console.WriteLine($"Sort by Name Using IComparer");
+            personList.Sort(new CompareByName());
+            Display(personList);
+        }
+
+        public static void Display(List<Person> personList)
+        {
+            personList.ForEach(person =>
+            {
+                Console.WriteLine($"Name { person.Name }  : Age {person.Age}");
+            });
+        }
+    }
+
+    public class Person : IComparable<Person>
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public int CompareTo(Person personToCompare)
+        {
+            if (personToCompare.Age != this.Age)
+            {
+                return personToCompare.Age.CompareTo(this.Age);
+            }
+            else
+            {
+                return this.Name.CompareTo(personToCompare.Name);
+            }
+        }
+    }
+
+    public class CompareByName : IComparer<Person>
+    {
+        public int Compare(Person x, Person y)
+        {
+            return string.Compare(x.Name, y.Name);
+        }
+    }
+
+    public class CompareByAge : IComparer<Person>
+    {
+        public int Compare(Person x, Person y)
+        {
+            if (x.Age > y.Age) return 1;
+            if (x.Age < y.Age) return 0;
+
+            return 0;
+        }
+    }
+}
---
+++ OOPSConcepts/Delegates/FuncDelegate.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace OOPSConcepts.Delegates
+{
+
+    public delegate int myDelegate(int x, int y);
+
+    public delegate T myGenericDelegate<T>(T args);
+
+    public class DelegateImplementation
+    {
+        private static DelegateImplementation _FuncDelegate = null;
+
+        public static DelegateImplementation Object
+        {
+            get
+            {
+                if (_FuncDelegate == null)
+                {
+                    _FuncDelegate = new DelegateImplementation();
+                }
+
+                return _FuncDelegate;
+            }
+        }
+
+     
+
+        public void DelegateOutput()
+        {
+            myDelegate myAddFunction = WorkerClass.Add;
+
+            int sum = myAddFunction(10, 30);
+
+            Console.WriteLine($"By Using Delegate {sum}");
+
+            myGenericDelegate<int> myGenericDelegate = WorkerClass.Sum;
+
+            Console.WriteLine($"By Using  Geniric Delegate {myGenericDelegate(40)}");
+
+        }
+
+
+        public void FunctionAndActionDelegate()
+        { 
+            Func<int,int,int> func = WorkerClass.Add;
+
+            Console.WriteLine($"By Using  Func {func(40,30)}");
+
+            //// Gives error
+            ////Func<int, int> func1 = WorkerClass.WithOutReturnTypeSum;
+            
+            Action action = WorkerClass.Nothing;
+            action();
+
+            Action<int, int> actionWithOutReturnType = WorkerClass.WithOutReturnTypeSum;
+            actionWithOutReturnType(50,30);
+
+
+
+        }
+    }
+
+    public static class WorkerClass
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int Sum(int x)
+        {
+            return 10 + x;
+        }
+
+        public static void Nothing()
+        {
+            Console.WriteLine($"Action : Method with out any return type");
+        }
+
+        public static void WithOutReturnTypeSum(int x , int y)
+        {
+            Console.WriteLine($"Action : Sum = {x + y}");
+        }
+    }
+}
